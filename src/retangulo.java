eda30209d2ed25eab1f172c2034a92998c8197c3
raphael101216 <raphaelrock_8@hotmail.com>
2020-05-20 19:31:56 -0300

    Projeto criado

---
+++ src/retangulo.java
@@ -0,0 +1,17 @@
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class retangulo {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.00");
+		
+		
+
+	}
+
+}

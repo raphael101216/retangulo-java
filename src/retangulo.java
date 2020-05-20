886f1cbe99a6becde21c5c945a85a6f3c8bef57f
raphael101216 <raphaelrock_8@hotmail.com>
2020-05-20 19:53:49 -0300

    Leitura da altura com validacao

--- src/retangulo.java
+++ src/retangulo.java
@@ -5,25 +5,35 @@ import java.util.Scanner;
 public class retangulo {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		DecimalFormat df = new DecimalFormat("0.00");
+
+		double larguraRetangulo, alturaRetanguo, area, perimetro;
+
+		// Leitura da largura com validacao"
+
+		System.out.print("Digite a largura do retangulo: ");
+		larguraRetangulo = sc.nextDouble();
+
+		while (larguraRetangulo <= 0) {
+
+			System.out.print("O valor deve ser positivo. Tente novamente: ");
+			larguraRetangulo = sc.nextDouble();
+		}
+
+		// Leitura da altura com validacao
+
+		System.out.print("Digite a altura do retangulo: ");
+		alturaRetanguo = sc.nextDouble();
+
+		while (alturaRetanguo <= 0) {
+
+			System.out.print("O valor deve ser positivo. Tente novamente: ");
+			alturaRetanguo = sc.nextDouble();
+		}
 		
-		  double larguraRetangulo, alturaRetanguo, area, perimetro;
-		  
-		  //Leitura da largura com validacao"
-		  
-		  System.out.print("Digite a largura do retangulo: ");
-		  larguraRetangulo = sc.nextDouble();
-		  
-		   while(larguraRetangulo <= 0) {
-			   
-			   System.out.print("O valor deve ser positivo. Tente novamente: ");
-			   larguraRetangulo = sc.nextDouble();
-		   }
-		   
-		   
 
 	}
 
